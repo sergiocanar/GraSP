34b94ec3a81efd5fdcbe852c1729ae45ad894c3e
sergiocanar <scanar@uniandes.edu.co>
2025-11-06 13:53:38 +0000

    add endoscapes_cutmargins dataset dataloader

--- TAPIS/tapis/datasets/__init__.py
+++ TAPIS/tapis/datasets/__init__.py
@@ -5,4 +5,5 @@ from .psi_ava import Psi_ava  # noqa
 from .grasp import Grasp  # noqa
 from .endovis_2017 import Endovis_2017
 from .endovis_2018 import Endovis_2018
+from .endoscapes2023_cutmargins import Endoscapes2023_cutmargins
 from .build import DATASET_REGISTRY, build_dataset  # noqa
